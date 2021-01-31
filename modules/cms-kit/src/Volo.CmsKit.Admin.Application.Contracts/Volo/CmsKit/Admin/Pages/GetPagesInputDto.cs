59086a5cfa3ce74185eab2b0c7d67a505841f97a
Alex-ling <mingzhao-ling@163.com>
2021-01-31 17:50:07 +0800

    Delete modules directory

--- modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Pages/GetPagesInputDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Application.Dtos;
-
-namespace Volo.CmsKit.Admin.Pages
-{
-    public class GetPagesInputDto : PagedAndSortedResultRequestDto
-    {
-        public string Filter { get; set; }
-    }
-}
--- modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Tags/TagGetListInput.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Application.Dtos;
-
-namespace Volo.CmsKit.Admin.Tags
-{
-    public class TagGetListInput : PagedAndSortedResultRequestDto
-    {
-        public string Filter { get; set; }
-    }
-}
